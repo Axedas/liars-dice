b1ba763deb8d5af2e68bed6c36955c594d2e3288
Quinn Baily <qbaily@arcadia.edu>
2020-12-01 19:12:22 -0500

    made method stubs

---
+++ models/Player.js
@@ -0,0 +1,31 @@
+class Player
+{
+    id;
+    dice;
+
+
+    constructor(id)
+    {
+        this.id = id;
+        this.dice = [0,0,0,0,0];
+    }
+
+    /**
+     * Player dices values reset
+     */
+    //rollDice(){}
+
+    //getDice(){}
+
+    //loseDie(){}
+    //pop() or set length
+
+    //gainDie(){}
+
+
+    //useTimeBank(){}
+
+
+
+
+}
